a689713da748e89881cdef176b40d0d9c335dc3b
charan028 <fancyviper007@gmail.com>
2025-09-14 13:08:32 -0500

    changes

--- demo.py
+++ demo.py
@@ -1,25 +1,18 @@
-import os
-import warnings
-import sys
-
-import pandas as pd
-import numpy as np
+import os, warnings, sys, logging
+import pandas as pd, numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
-from urllib.parse import urlparse
-import mlflow
-from mlflow.models.signature import infer_signature
-import mlflow.sklearn
-import dagshub
-import logging
+import mlflow, mlflow.sklearn
 import dagshub
 
+# Initialize DagsHub & MLflow correctly
 dagshub.init(repo_owner='mohamedabrar786786', repo_name='MLFLOW_DAGSUB', mlflow=True)
+# If you prefer to be explicit, uncomment the next line (but it's redundant with mlflow=True above):
+# mlflow.set_tracking_uri("https://dagshub.com/mohamedabrar786786/MLFLOW_DAGSUB.mlflow")
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
-
 
 def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
@@ -27,73 +20,42 @@ def eval_metrics(actual, pred):
     r2 = r2_score(actual, pred)
     return rmse, mae, r2
 
-
-
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
 
-    # Read the wine-quality csv file from the URL
-    csv_url = (
-        "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv"
-    )
+    csv_url = "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv"
     try:
         data = pd.read_csv(csv_url, sep=";")
     except Exception as e:
-        logger.exception(
-            "Unable to download training & test CSV, check your internet connection. Error: %s", e
-        )
+        logger.exception("Unable to download CSV: %s", e)
+        raise
 
-    # Split the data into training and test sets. (0.75, 0.25) split.
-    train, test = train_test_split(data)
-
-    # The predicted column is "quality" which is a scalar from [3, 9]
-    train_x = train.drop(["quality"], axis=1)
-    test_x = test.drop(["quality"], axis=1)
-    train_y = train[["quality"]]
-    test_y = test[["quality"]]
-
+    train, test = train_test_split(data, test_size=0.25, random_state=42)
+    X_train, X_test = train.drop(columns=["quality"]), test.drop(columns=["quality"])
+    y_train, y_test = train["quality"].values.ravel(), test["quality"].values.ravel()
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
+    # (Optional) pick a consistent experiment
+    mlflow.set_experiment("default")
 
-
+    # Start the run AFTER the tracking URI is set
     with mlflow.start_run():
-        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-        lr.fit(train_x, train_y)
+        model = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        model.fit(X_train, y_train)
+        preds = model.predict(X_test)
 
-        predicted_qualities = lr.predict(test_x)
+        rmse, mae, r2 = eval_metrics(y_test, preds)
+        print(f"Elasticnet model (alpha={alpha:.6f}, l1_ratio={l1_ratio:.6f}):")
+        print(f"  RMSE: {rmse}")
+        print(f"  MAE:  {mae}")
+        print(f"  R2:   {r2}")
 
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        mlflow.log_params({"alpha": alpha, "l1_ratio": l1_ratio})
+        mlflow.log_metrics({"rmse": rmse, "mae": mae, "r2": r2})
 
-        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
-
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
-
-        
-        # For remote server only (Dagshub)
-        remote_server_uri = "https://dagshub.com/api/v1/repo-buckets/s3/mohamedabrar786786"
-        mlflow.set_tracking_uri(remote_server_uri)
-
-
-
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-
-        # Model registry does not work with file store
-        if tracking_url_type_store != "file":
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(
-                lr, "model", registered_model_name="ElasticnetWineModel")
-        else:
-            mlflow.sklearn.log_model(lr, "model")
+        # Log & (optionally) register the model
+        mlflow.sklearn.log_model(model, artifact_path="model",
+                                 registered_model_name="ElasticnetWineModel")
